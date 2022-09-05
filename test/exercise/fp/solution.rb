7565a3062c72b67d262540cc259b1090813ac2bd
Александр Михайлов <meh@MacBook-Air-Aleksandr.local>
2022-09-05 09:45:48 +0300

    exercise fp

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,12 +4,18 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        arr_filter_rating = array
+                            .select { |el| el['rating_kinopoisk'] != '0' && !el['rating_kinopoisk'].nil? }
+                            .select { |el| !el['country'].nil? && el['country'].include?(',') }
+
+        arr_filter_rating.reduce(0) { |acc, el| acc + el['rating_kinopoisk'].to_f } / arr_filter_rating.size
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        films.select { |el| el['rating_kinopoisk'].to_f >= threshold }
+             .map { |el| { 'name' => el['name'] } }
+             .reduce(0) { |acc, el| acc + el['name'].count('и') }
       end
     end
   end
