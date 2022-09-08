36ae919c12cb2c2808090ceaf19a8d6ef428a8b6
MehPNZ <mikhaylov.av22@gmail.com>
2022-09-08 13:34:45 +0300

    refactoring fp2

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,35 +5,32 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each
-        for i in self do
-          yield i
-        end
+      def my_each(&block)
+        first, *rest = self
+        block.call(first)
+        rest.size.zero? ? [first] : [first, *MyArray.new(rest).my_each(&block)]
       end
 
       # Написать свою функцию my_map
-      def my_map
-        result = MyArray.new
-        my_each { |elem| result << yield(elem) }
-        result
+      def my_map(&block)
+        MyArray.new(my_reduce([]) { |acc, el| acc << block.call(el) })
       end
 
       # Написать свою функцию my_compact
       def my_compact
-        result = MyArray.new
-        my_map { |el| result << el unless el.nil? }
-        result
+        MyArray.new(my_reduce([]) { |acc, el| el.nil? ? acc : acc << el })
       end
 
       # Написать свою функцию my_reduce
-      def my_reduce(acc = nil)
+      def my_reduce(acc = nil, &block)
+        first, *rest = self
         if acc.nil?
-          acc = self[0]
-          MyArray.new(self[1..size - 1]).my_map { |el| acc = yield acc, el }
+          acc = block.call(first, rest[0])
+          return MyArray.new(rest[1..(rest.size - 1)]).my_reduce(acc, &block)
         else
-          my_map { |el| acc = yield acc, el }
+          acc = block.call(acc, first)
         end
-        acc
+        rest.size.zero? ? acc : MyArray.new(rest).my_reduce(acc, &block)
       end
     end
   end
