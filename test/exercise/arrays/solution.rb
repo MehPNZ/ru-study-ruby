f1f1af771775569980786284af4019e49c0fbfd9
MehPNZ <mikhaylov.av22@gmail.com>
2022-09-08 08:22:09 +0300

    refactoring feature/arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -3,27 +3,21 @@ module Exercise
     class << self
       def replace(array)
         max_el = array[0]
-        count = array.size - 1
-
-        (max_el <=> array[count]) == -1 ? max_el = array[count] : count -= 1 while count != 0
-
-        array.map { |el| el.positive? ? max_el : el }
+        array.each { |el| max_el = el if (max_el <=> el) == -1 }
+             .map { |el| el.positive? ? max_el : el }
       end
 
-      def search(array, query)
-        arr_sort = array.sort
+      def search(array, query, left = 0, right = array.size - 1)
+        return -1 if (left > right) || array.empty?
 
-        iter = lambda { |low, high, mid|
-          return -1 if low > high
-          return mid if arr_sort[mid] == query
+        mid = (left + right) / 2
+        return mid if query == array[mid]
 
-          if query > arr_sort[mid]
-            iter.call(mid + 1, high, (low + + high) / 2)
-          else
-            iter.call(low, mid - 1, (low + high) / 2)
-          end
-        }
-        iter.call(0, array.size - 1, arr_sort.size / 2)
+        if query > array[mid]
+          search(array, query, mid + 1, right)
+        else
+          search(array, query, left, mid - 1)
+        end
       end
     end
   end
